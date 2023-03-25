8de26b384e1b6340537c113cfff5e79a8a7ca299
jllrmb24 <114567613+jalilahmadi1234567890@users.noreply.github.com>
2023-03-25 15:45:31 +0700

    Add files via upload

---
+++ praktikum/p4_jalil_si14/function.php
@@ -0,0 +1,14 @@
+<?php
+include "library.php";
+require "rumus.php";
+
+
+hitungLuasPersegi(20);
+
+echo"<br>";
+
+hitungLuasPersegiPanjang(20,40);
+
+echo"<br>";
+
+hitungLuasLingkaran(30);
---
+++ praktikum/p4_jalil_si14/rumus.php
@@ -0,0 +1,12 @@
+<?php
+
+function hitungLuasPersegiPanjang($panjang, $lebar)
+{
+    echo $panjang * $lebar;
+}
+
+ function hitungLuasLingkaran($jariJari)
+ {
+      define('PHI', 3.14);
+      echo PHI *$jariJari*$jariJari;
+ }
