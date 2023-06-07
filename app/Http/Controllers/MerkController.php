d17588e5d253646fcc961172b77b6f4820674fa7
jalilahmadi1234567890 <jalilrambe24@gmail.com>
2023-06-07 15:51:41 +0700

    lanjutan

--- app/Http/Controllers/MerkController.php
+++ app/Http/Controllers/MerkController.php
@@ -26,4 +26,26 @@ class MerkController extends Controller
 
         return redirect()->to('/merk')->with('success', 'Data berhasil disimpan');
     }
+
+    function edit($id)
+    {
+        $merkData = Merk::find($id); //SELECT *FROM merks WHERE id = $id
+        return view('pages.merk.edit', ['merkData' => $merkData]);
+    }
+    function update($id, Request $request)
+    {
+        $merkData = Merk::find($id);
+        $merkData->merk = $request->merk;
+        $merkData->save();
+
+        return redirect()->to('/merk')->with('success', 'Data berhasil diupdate');
+    }
+
+    function delete($id)
+    {
+        $merkData = Merk::find($id);
+        $merkData->delete();
+
+        return redirect()->to('/merk')->with('success', 'Data merk berhasil dihapus');
+    }
 }
